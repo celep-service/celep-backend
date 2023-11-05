1dbe1357159d6850727993d9f53ba0ada521ba18
박창현 <86464544+Kid-Chang@users.noreply.github.com>
2023-11-05 12:44:47 +0900

    init

---
+++ src/main/java/com/cody/springcody/SpringCodyApplication.java
@@ -0,0 +1,13 @@
+package com.cody.springcody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCodyApplication.class, args);
+	}
+
+}
