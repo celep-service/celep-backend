bc15a3dad47e4a4dcbb6d3499862b7cdcb08df12
Kid-Chang <pch3310@gmail.com>
2023-11-26 19:33:32 +0900

    docs: 스웨거 문서 기본 설정

--- src/main/java/com/celeb/post/PostController.java
+++ src/main/java/com/celeb/post/PostController.java
@@ -1,6 +1,8 @@
 package com.celeb.post;
 
 import com.celeb._base.dto.DataResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -15,10 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/posts")
+@Tag(name = "게시글 관련 API", description = "게시글 관련 API")
 public class PostController {
 
     private final PostService postService;
 
+    @Operation(summary = "포스트 정보 조회", description = "회원 정보가 삭제됩니다.", tags = {"게시글 관련 API"})
     @GetMapping("")
     public DataResponseDto<Object> getPosts(
         @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
