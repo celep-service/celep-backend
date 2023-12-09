b87b0820ebc193c0fc8a825283504e1dede15663
Kid-Chang <pch3310@gmail.com>
2023-12-09 23:22:18 +0900

    CORS setting

---
+++ src/main/java/com/celeb/_base/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.celeb._base.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//            .allowedOrigins("https://celep.shop")
+//            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+//            .allowedHeaders("Authorization", "Content-Type")
+//            .maxAge(3600)
+//            .allowCredentials(true);
+//    }
+}
--- src/main/java/com/celeb/security/config/SecurityConfig.java
+++ src/main/java/com/celeb/security/config/SecurityConfig.java
@@ -15,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
@@ -48,8 +51,23 @@ public class SecurityConfig {
                     .requestMatchers(HttpMethod.PATCH, "/posts/**",
                         "/clothes/**", "/comments/**").authenticated()
                     .anyRequest().permitAll()
-            );
+            )
+            .cors((cors) -> cors.configurationSource(corsConfigurationSource()));
 
         return http.build();
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("https://celep.shop");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
