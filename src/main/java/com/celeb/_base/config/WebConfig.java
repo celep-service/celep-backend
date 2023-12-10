b21d70805a34c49edd462d4d13affe1b1c515a68
Kid-Chang <pch3310@gmail.com>
2023-12-10 13:07:44 +0900

    fix: post 메서드 에러 수정?

--- src/main/java/com/celeb/_base/config/WebConfig.java
+++ src/main/java/com/celeb/_base/config/WebConfig.java
@@ -1,18 +1,19 @@
 package com.celeb._base.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//            .allowedOrigins("https://celep.shop")
-//            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-//            .allowedHeaders("Authorization", "Content-Type")
-//            .maxAge(3600)
-//            .allowCredentials(true);
-//    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://celep.shop")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+            .allowedHeaders("Authorization", "Content-Type")
+            .maxAge(3600)
+            .allowCredentials(true);
+    }
 }
