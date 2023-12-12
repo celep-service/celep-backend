1024e3bb2057f7283088e7597c36075468888b30
donghun1214 <acekdhh@naver.com>
2023-12-12 23:17:55 +0900

    feat: [POST][GET] /bookmarks/post 북마크 업데이트, 조회 추가

---
+++ src/main/java/com/celeb/postBookmark/PostBookmarkRepository.java
@@ -0,0 +1,21 @@
+package com.celeb.postBookmark;
+
+import com.celeb.post.Post;
+import com.celeb.user.User;
+import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostBookmarkRepository extends JpaRepository<PostBookmark, Integer> {
+    Optional<PostBookmark> findByUserAndPost(User user, Post post);
+
+    @Query("SELECT p FROM PostBookmark pb JOIN pb.post p WHERE pb.user = :user")
+    Slice<Post> findPostByMember(@Param("user") User user, Pageable pageable);
+
+    Long countByPost(Post post);
+}
